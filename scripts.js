2b3c6ddd82f9d5027c31208c1e1ca7719f71202b
AdelinoJR76 <afsjr.bh@gmail.com>
2022-04-01 17:04:03 -0300

    first commit conta corrente em JS com orientacao a objeto

---
+++ scripts.js
@@ -0,0 +1,64 @@
+class contaBancaria{
+    constructor(agencia,numero,tipo,saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = 0 ;
+    }
+    get saldo(){
+        return this._saldo ;
+    }
+    set saldo(valor){
+        return this._saldo = valor; 
+    } 
+
+    sacar(valor){
+        if(valor > this._saldo){
+            return 'Operação Negada';
+        } 
+        this._saldo -=valor;
+        return this._saldo;
+    }
+
+    deposita(valor){
+        this._saldo +=valor;
+        return this._saldo;
+    }
+}
+
+class contaCorrente extends contaBancaria{
+    constructor(agencia,numero,cartaocredito){
+        super(agencia,numero);
+        this.tipo = 'corrente';
+        this._cartaoCredito = 'cartaocredito';
+    }   
+
+    get cartaoCredito(){
+        return this._cartaoCredito; 
+    }
+
+    set cartaoCredito(valor) {
+        this._cartaoCredito = valor;
+    }
+}
+
+class contaPoupanca extends contaBancaria{
+    constructor(agencia,numero){
+        super(agencia,numero);
+        this.tipo = 'poupanca';
+    }
+}
+
+class contaUniversitaria extends contaBancaria {
+    constructor(agencia,numero){
+        super(agencia,numero);
+        this.tipo = 'universitaria';
+    }
+    sacar(valor){
+        if (valor > 500) {
+            return 'operação negada'
+        }
+        this._saldo = this._saldo - valor;
+        }
+
+}
